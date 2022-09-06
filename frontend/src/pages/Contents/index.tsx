788f3e75d90977b28c665f9a48b5b6020c75e026
riquelme <riquelmebandeira1@outlook.com>
2022-09-05 21:15:22 -0300

    feat(interface): user can add a new content

--- frontend/src/pages/Contents/index.tsx
+++ frontend/src/pages/Contents/index.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Card from '../../components/Card'
+import PopupForm from '../PopupForm'
+import api from '../../services/api'
+import './styles.scss'
 
-const Content = () => {
+const Content: React.FC = () => {
+  const [contents, setContents] = useState<any[]>([])
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    api.get('/contents')
+      .then((response) => setContents(response.data))
+  }, [])
+
   return (
     <div>
-      Conteúdos
+      <header className="header">
+        <h1 className="header__text">Desafio Motrix</h1>
+      </header>
+
+      <button
+        className='btn-create'
+        onClick={() => setShow(true)}
+      >
+        Novo conteúdo
+      </button>
+
+      <PopupForm
+        heading="Criar conteúdo"
+        show={show}
+        setShow={setShow}
+        onClick={
+          (title: string, body: string) =>
+            api.post('/contents', { title, body })
+              .then(({ data }) => setContents([...contents, data]))
+        }
+       />
+
+      <section className="contents">
+        {
+          contents && (
+            contents.map(({ _id: id, title }) => (
+              <Card key={id} id={id} title={title} />
+            ))
+          )
+        }
+      </section>
     </div>
   )
 }
